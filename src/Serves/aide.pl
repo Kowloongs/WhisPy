c842993fc15e58f3146b50301c9453af1e9757de
Carlyon W. Tung <loongs@syuusui.com>
2018-06-21 00:06:52 +0800

    aide

--- src/Serves/aide.pl
+++ src/Serves/aide.pl
@@ -1,9 +1,9 @@
 /*
 ****************************************************************************************************************
-*= = 项目名称: The Tips is coming from WhisPy Project.
-*= = 运行环境: Please load this file into Insight Prolog.
-*= = 编码人员: Carlyon W. Tung
-*= = 编码时间: 2018-06-09 07:03:00
+*= = 项目名称：The Tips is coming from WhisPy Project.
+*= = 运行环境：Please load this file into Insight Prolog.
+*= = 编码人员：Carlyon W. Tung
+*= = 编码时间：2018-06-09 07:03:00
 ****************************************************************************************************************
 */
 
@@ -17,14 +17,14 @@ server(Port) :-
 
 aide_say_hi(_Request) :-
     format('Content-type: text/plain~n~n'),
-    format('Welcome, I am Pytalk!~n~n').
+    format('Welcome, I am WhisPy!~n~n').
 
 :- server(52615).
 
 
 /*
 ****************************************************************************************************************
-*= = End of File
+*= = End of file
 ****************************************************************************************************************
 */
 
