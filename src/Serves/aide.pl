4d2dfe6668a212213e0de2ca80362aecada0c489
Carlyon W. Tung <loongs@syuusui.com>
2018-06-21 00:16:44 +0800

    aide

--- src/Serves/aide.pl
+++ src/Serves/aide.pl
@@ -1,6 +1,6 @@
 /*
 ****************************************************************************************************************
-*= = 项目名称：The Tips is coming from WhisPy Project.
+*= = 项目名称：The aide is coming from WhisPy Project.
 *= = 运行环境：Please load this file into Insight Prolog.
 *= = 编码人员：Carlyon W. Tung
 *= = 编码时间：2018-06-09 07:03:00
